a35f760e1d9cbd986c84822a2287cb3a87827249
shabnam <shabnam.mostary@gmail.com>
2020-11-12 23:40:20 -0500

    implemnted the api createNewMovie

--- controller/movies.js
+++ controller/movies.js
@@ -1,7 +1,22 @@
-const { request, response } = require('express')
 const movies = require('../movies')
 const getAllMovies = (request, response) => {
   return response.send(movies)
 }
+const createNewMovie = (request, response) => {
+  const {
+    title, directors, releaseDate, rating, runTime, genres
+  } = request.body
 
-module.exports = { getAllMovies }
+  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
+    return response.status(400).send('Following items are required title,directors,releaseDate,rating,runtime & genres')
+  }
+  const newMovie = {
+    title, directors, releaseDate, rating, runTime, genres
+  }
+
+  movies.push(newMovie)
+
+  return response.status(201).send(newMovie)
+}
+
+module.exports = { getAllMovies, createNewMovie }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { getAllMovies } = require('./controller/movies')
+const bodyParser = require('body-parser')
+const { getAllMovies, createNewMovie } = require('./controller/movies')
 const app = express()
 
 app.get('/movies', getAllMovies)
+app.post('/movies', bodyParser.json(), createNewMovie)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337')// eslint-disable-line no-console
