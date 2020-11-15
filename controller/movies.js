dee8bd0f8aa4da387218cd5e4e3b055db7dbf4b7
shabnam <shabnam.mostary@gmail.com>
2020-11-15 17:11:45 -0500

    simplified the code of getMovie api

--- controller/movies.js
+++ controller/movies.js
@@ -5,24 +5,25 @@ const getAllMovies = (request, response) => {
 
 const getMovie = (request, response) => {
   const { input } = request.params
-  let result = []
-  const result1 = movies.filter((movie) => movie.title.toUpperCase().includes(input.toUpperCase()))
 
-
-  const result2 = movies.filter((movie) => {
-    for (let i = 0; i < movie.directors.length; i++) {
+  const foundMovie = movies.filter((movie) => { // filter function is for each movie.line 10-19 will run for each movie.
+    if (movie.title.toUpperCase().includes(input.toUpperCase())) { // search for input in the title,return true if found
+      return true // if the condition is false it will enter the for loop next.If true, it will move to next movie.
+    }
+    for (let i = 0; i < movie.directors.length; i++) { // search for input in the the directors array,return true if found
       if (movie.directors[i].toUpperCase().includes(input.toUpperCase())) {
         return true
       }
     }
 
-    return false
+    return false // return false if input is not in the movie title or directors array
   })
 
-  result = result1.concat(result2)
-  const foundMovie = result.filter((v, i, a) => a.indexOf(v) === i)
-
-  return response.send(foundMovie)
+  if (foundMovie.length > 1) {
+    return response.send(foundMovie)
+  } else {
+    return response.send('No movies found!')
+  }
 }
 const createNewMovie = (request, response) => {
   const {
@@ -30,7 +31,7 @@ const createNewMovie = (request, response) => {
   } = request.body
 
   if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
-    return response.status(400).send('Following items are required title,directors,releaseDate,rating,runtime & genres')
+    return response.status(400).send('Following items are required title,directors,releaseDate,rating,runTime & genres')
   }
   const newMovie = {
     title, directors, releaseDate, rating, runTime, genres
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/movies', getAllMovies)
 app.get('/movies/:input', getMovie)
 app.post('/movies', bodyParser.json(), createNewMovie)
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
 
 app.listen(1337, () => {
   console.log('Listening on port 1337')// eslint-disable-line no-console
