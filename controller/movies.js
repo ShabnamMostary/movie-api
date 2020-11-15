94f56dbeef89bb3c120ce94555b24971b8082b39
shabnam <shabnam.mostary@gmail.com>
2020-11-14 19:59:19 -0500

    api for search by directors & title

--- controller/movies.js
+++ controller/movies.js
@@ -5,8 +5,21 @@ const getAllMovies = (request, response) => {
 
 const getMovie = (request, response) => {
   const { input } = request.params
+  const foundMovie = []
+  const result1 = movies.filter((movie) => movie.title.toUpperCase().includes(input.toUpperCase()))
 
-  const foundMovie = movies.filter((movie) => movie.title.toUpperCase().includes(input.toUpperCase()))
+  foundMovie.push(result1)
+  const result2 = movies.filter((movie) => {
+    for (let i = 0; i < movie.directors.length; i++) {
+      if (movie.directors[i].toUpperCase().includes(input.toUpperCase())) {
+        return true
+      }
+    }
+
+    return false
+  })
+
+  foundMovie.push(result2)
 
   return response.send(foundMovie)
 }
